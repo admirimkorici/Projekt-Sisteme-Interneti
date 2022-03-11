e80f72d13b603beab3bba349e662290c74903f49
Admirim Korici <82025628+admirimkorici@users.noreply.github.com>
2022-03-12 00:20:12 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projekt_Sisteme_Interneti.Startup))]
+namespace Projekt_Sisteme_Interneti
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
